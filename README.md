096e53bf2105fdc9317caaadc80f4156c3ba8364
mhollow56 <mikeholloway7023@att.net>
2022-10-28 19:34:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nznmpt-1yeuuj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nznmpt-1yeuuj)
---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Laptop',
+    price: 800,
+    description: ' good dependable laptop',
+  },
+  {
+    id: 2,
+    name: 'Desktop ',
+    price: 1000,
+    description: 'A gaming computer with all the bells and whistles',
+  },
+  {
+    id: 3,
+    name: 'Tablet',
+    price: 299,
+    description: '',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
